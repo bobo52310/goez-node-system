30060952c7ed164d98186746b3ced8d6198fa07e
Jace Ju <jaceju@gmail.com>
2013-12-10 17:07:06 +0800

    Add prefix for tables.

--- src/migrations/2013_12_10_072245_create_node_system_tables.php
+++ src/migrations/2013_12_10_072245_create_node_system_tables.php
@@ -15,7 +15,7 @@ class CreateNodeSystemTables extends Migration
     {
         // fields: 實際存放 field 資料的表格，例如一篇文章的段落內容、圖片資訊、上傳檔案資訊等。
         // nodes 與 fields 為一對多關係。
-        Schema::create('fields', function (Table $table) {
+        Schema::create('goez_fields', function (Table $table) {
             $table->increments('id');
             $table->integer('node_id');
             $table->integer('type_id');
@@ -26,7 +26,7 @@ class CreateNodeSystemTables extends Migration
         });
 
         // field_types: 存放目前有哪些 field 類型的表格，例如：段落、圖片、上傳檔案、評論、廣告連結等。
-        Schema::create('field_types', function (Table $table) {
+        Schema::create('goez_field_types', function (Table $table) {
             $table->increments('id');
             $table->string('type', 100);
             $table->string('name', 100);
@@ -34,7 +34,7 @@ class CreateNodeSystemTables extends Migration
         });
 
         // nodes: 實際存放 node 資料的表格，例如每篇文章的標題、描述、發佈時間等。
-        Schema::create('nodes', function (Table $table) {
+        Schema::create('goez_nodes', function (Table $table) {
             $table->increments('id');
             $table->integer('user_id');
             $table->string('type', 100);
@@ -47,7 +47,7 @@ class CreateNodeSystemTables extends Migration
         });
 
         // node_types: 存放目前有哪些 node 類型的表格，例如：關於 KKBOX 、風雲榜、廣告。
-        Schema::create('node_types', function (Table $table) {
+        Schema::create('goez_node_types', function (Table $table) {
             $table->increments('id');
             $table->string('type', 100); // Node 類型的英文名稱
             $table->string('name', 100); // Node 類型的中文名稱
@@ -56,14 +56,14 @@ class CreateNodeSystemTables extends Migration
         });
 
         // node_tags: 標註 nodes 與 tags 的關係。
-        Schema::create('node_tags', function (Table $table) {
+        Schema::create('goez_node_tags', function (Table $table) {
             $table->increments('id');
             $table->integer('node_id'); // 對應到 nodes.id
             $table->integer('tag_id'); // 對應到 tags.id
         });
 
         // tags: 用以對 node 做分類的表格。
-        Schema::create('tags', function (Table $table) {
+        Schema::create('goez_tags', function (Table $table) {
             $table->increments('id');
             $table->string('name', 100); // 標籤名稱
         });
