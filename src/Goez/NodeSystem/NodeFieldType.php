6f1d5cc8fa5c8f24539026a2eee754cd3036725e
Jace Ju <jaceju@gmail.com>
2013-12-13 11:16:13 +0800

    Update schema of node-field-types.

--- src/Goez/NodeSystem/NodeFieldType.php
+++ src/Goez/NodeSystem/NodeFieldType.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Schema\Blueprint as Table;
 class NodeFieldType extends Eloquent
 {
     public static $tableName = 'goez_node_field_types';
+    public $timestamps = false;
     protected $table = 'goez_node_field_types';
+    protected $guarded = array();
 
     /**
      * @return callable
@@ -17,8 +19,9 @@ class NodeFieldType extends Eloquent
     {
         return function (Table $table) {
             $table->increments('id');
-            $table->integer('node_id'); // 對應到 nodes.id
+            $table->integer('node_type_id'); // 對應到 nodes_types.id
             $table->integer('field_type_id'); // 對應到 field_types.id
+            $table->string('name', 100); // 欄位名稱
         };
     }
 }
