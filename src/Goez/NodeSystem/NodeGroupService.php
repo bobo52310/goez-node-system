68a82e06eddf864f5dddc73f9fe0b24f810c02d8
andytsai <andytsai@kkbox.com>
2014-10-09 16:02:15 +0800

    Add detection of duplicate language names.

--- src/Goez/NodeSystem/NodeGroupService.php
+++ src/Goez/NodeSystem/NodeGroupService.php
@@ -3,32 +3,74 @@
 namespace Goez\NodeSystem;
 
 use Goez\NodeSystem\NodeGroup as NodeGroup;
+use Goez\NodeSystem\NodeLanguage as NodeLanguage;
 
 class NodeGroupService
 {
     protected $nodeGroup;
 
-    public function __construct(NodeGroup $nodeGroup)
+    protected $nodeLanguage;
+
+    public function __construct(NodeGroup $nodeGroup, NodeLanguage $nodeLanguage)
     {
-        $this->nodeGroup = $nodeGroup;
+        $this->nodeGroup    = $nodeGroup;
+        $this->nodeLanguage = $nodeLanguage;
     }
 
-    public function create(array $attributes)
+    public function create($nodeId, $groupId)
     {
-        $nodeGroup = $this->nodeGroup->create($attributes);
+        if ($this->languageDuplicate([$nodeId], $groupId)) {
+            return false;
+        }
 
-        return $nodeGroup;
+        return $nodeGroup = $this->nodeGroup->create([
+            $this->getNodeIdColumn()  => $nodeId,
+            $this->getGroupIdColumn() => $groupId
+        ]);
     }
 
-    public function batchCreate(array $values)
+    public function update($nodeId, $groupId)
     {
-        $create = $this->nodeGroup->insert($values);
+        if ($this->languageDuplicate([$nodeId], $groupId)) {
+            return false;
+        }
+
+        $nodeGroup = $this->findByNode($nodeId);
+
+        if (empty($nodeGroup)) {
+            return false;
+        }
+
+        return $nodeGroup->update([
+            $this->getGroupIdColumn() => $groupId
+        ]);
+    }
+
+    public function batchCreate(array $nodeIds, $groupId)
+    {
+        if ($this->languageDuplicate($nodeIds, $groupId)) {
+            return false;
+        }
+
+        $nodeGroups = [];
+        foreach ($nodeIds as $nodeId) {
+            $nodeGroups[] = [
+                $this->getNodeIdColumn()  => $nodeId,
+                $this->getGroupIdColumn() => $groupId
+            ];
+        }
+
+        $create = $this->nodeGroup->insert($nodeGroups);
 
         return $create;
     }
 
-    public function batchUpdate($nodeIds, $groupId)
+    public function batchUpdate(array $nodeIds, $groupId)
     {
+        if ($this->languageDuplicate($nodeIds, $groupId)) {
+            return false;
+        }
+
         $query = $this->nodeGroup->query();
 
         $query->whereIn($this->nodeGroup->getNodeIdColumn(), $nodeIds);
@@ -97,5 +139,21 @@ class NodeGroupService
     public function getTable()
     {
         return $this->nodeGroup->getTable();
+    }
+
+    public function languageDuplicate(array $nodeIds, $groupId)
+    {
+        $nodeIdColumn  = $this->getNodeIdColumn();
+        $groupIdColumn = $this->getGroupIdColumn();
+
+        $allNodeIds = array_merge(
+            $nodeIds,
+            $this->getByGroup($groupId)->lists($nodeIdColumn)
+        );
+
+        $nodeLanguages = $this->nodeLanguage->query()->whereIn($nodeIdColumn, $allNodeIds)->lists('language_name');
+
+        /** Check for duplicates. */
+        return count($nodeLanguages) !== count(array_unique($nodeLanguages));
     }
 }
